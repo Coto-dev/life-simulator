7428706526598f472f74872abef845ce9beee15b
coto1488 <shorop5@mail.ru>
2022-09-22 22:16:26 +0700

    startup

---
+++ life-simulator/Classes/Animal/Animal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using life_simulator.Classes.abstractCreature;
+
+namespace life_simulator.Classes.Animal
+{
+    internal class Animal : Creature, IAlive
+    {
+        public void die()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool isHungry()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void move()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void pair<T>(T partner)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void searchFood<T>(T food)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    enum AnimalTypes
+    {
+        Predator,
+        Herbivorous
+    }
+}
---
+++ life-simulator/Classes/abstractCreature/Creature.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace life_simulator.Classes.abstractCreature
+{
+    internal abstract class Creature
+    {
+        protected int hp;
+        protected int satiety;
+        protected  <T> sex;
+    }
+}
