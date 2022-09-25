5ba8d79ae33b7046928c807a4ea2cb6713b2d74a
iseeyouwatching <leonidshibanovv@gmail.com>
2022-09-25 12:16:58 +0700

    Template

---
+++ life-simulator/Classes/Plant/Plant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace life_simulator.Plant
+{
+    internal class Plant
+    {
+        public bool isEdible;
+        public bool isGrown;
+
+        private void grow()
+        {
+
+        }
+    }
+}
