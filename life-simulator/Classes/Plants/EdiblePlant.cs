cd9a908586bd5c1d1d3414943b799d78bed63ce6
iseeyouwatching <leonidshibanovv@gmail.com>
2022-09-27 17:56:32 +0700

    Class Plant

---
+++ life-simulator/Classes/Plants/EdiblePlant.cs
@@ -0,0 +1,14 @@
+﻿using life_simulator.Render;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace life_simulator.Plants
+{
+	internal class EdiblePlant : Plant {
+		public EdiblePlant(World world) : base(world) {
+		}
+	}
+}
---
+++ life-simulator/Classes/Plants/Plant.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using life_simulator.Classes;
+using life_simulator.Render;
+using System.Drawing;
+
+namespace life_simulator.Plants
+{
+    public class Plant : Entity
+    {
+        public bool isEdible;
+        public bool isGrown;
+
+		public Plant(World world) : base(world) {
+			isFreezed = true;
+			Render.SetColor(Color.Yellow);
+			Render.SetSvg("assets/svg/Plant.svg");
+			Render.Rerender();
+		}
+
+		override public void Tick() {
+			base.Tick();
+			if (Ticks == 100) {
+				grow();
+				Console.WriteLine(Ticks);
+			}
+		}
+
+		private void grow()
+        {
+			isEdible = true;
+			isGrown = true;
+			Render.SetColor(Color.Green);
+			Render.Rerender();
+		}
+	}
+}
